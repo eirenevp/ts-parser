c3ac01d090ec133d1a7bcc7ede78402dba95adfd
irenevp <irene.vlassi@gmail.com>
2020-12-10 00:40:33 +0000

    Update

--- parser-constraints.sh
+++ parser-constraints.sh
@@ -3,7 +3,7 @@
 printf "Script for parsing constraints.\n"
 printf "File,Constraints\n" > parsedConstraints.csv
 
-for f in $(find /Users/iren/Code/opttyper/Evaluation/PureTypeScript -name '*constraints.json'); 
+for f in $(find /Users/iren/Code/opttyper/Evaluation/modules -name '*constraints.json'); 
 do 
     python "src/parser-constraints.py" --in_file=${f} --out_file="/Users/iren/Code/ts-parser/parsedConstraints.csv"
 done
--- parser-opt.sh
+++ parser-opt.sh
@@ -6,7 +6,7 @@ printf "Script for parsing files.\n"
 # rm parsedFunctions.csv
 printf "File,FnId,Type\n" > parsedFunctionsOpt.csv
 
-for f in $(find ../opttyper/Evaluation/PureTypeScript -name 'opttyper.*.ts'); 
+for f in $(find ../opttyper/Evaluation/modules -name 'opttyper.*.ts'); 
 do 
     # printf "${f#../test-repos/}," >> parsedFunctions.csv 
     node ${DIST_PATH}/parser.js ${f} parsedFunctionsOpt.csv
